cd20b3141626559f7725d5b4b92aca74b685f048
Baveja <munish.sharma46@t-mobile.com (munish.sharma46@t-mobile.com)>
2017-01-26 20:07:37 +0530

    commit

--- app.node.js
+++ app.node.js
@@ -226,7 +226,11 @@ app.get('/apiDetails/:id',function(req,res){
 
 
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+var server = app.listen(8081, function () {
+
+	 
+
+	  console.log("Example app listening at http://%s:%s", host, port)
+
+	})
 
